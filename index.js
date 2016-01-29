c4995f79812f321649ac66d8427a3e2b4d6ed7b4
James Wright <james.wright@net-a-porter.com>
2016-01-29 11:02:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var NodeCache = require('node-cache');
+
+function CappedCache(options) {
+	this._maxSize = options.maxSize;
+	this._cache = new NodeCache({ stdTTL: options.defaultTTL, useClones: false });
+}
+
+CappedCache.prototype.get = function get(cacheKey) {
+	return this._cache.get(cacheKey);
+};
+
+CappedCache.prototype.set = function set(cacheKey, data, ttl) {
+	var shouldCache = !this._isFull()
+	
+	if (shouldCache) {
+		this._cache.set(cacheKey, data, ttl);
+	}
+	
+	return shouldCache;
+};
+
+CappedCache.prototype._isFull = function isFull() {
+	if (!this._maxSize) {
+		return false;
+	}
+	
+	return this._cache.getStats().keys <= this._maxSize();
+}
+
+CappedCache.prototype.reset = function reset() {
+	this._cache.flushAll();
+};
+
+module.exports = CappedCache;
